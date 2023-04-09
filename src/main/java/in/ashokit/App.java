99bb5c5e1c51a39966ce423075eafd4d8574ce2c
Diksha <dikshak1611@gmail.com>
2023-04-09 23:10:40 +0530

    Bug fix-123

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -6,13 +6,31 @@ package in.ashokit;
  */
 public class App 
 {
+
+    public int add(int a,int b){
+	int c=a+b;
+	return c;
+}
+
+//Bug-123
+
+public int sub(int a,int b){
+	//in progress
+}
     public static void main( String[] args )
     {
+
 		int a=10;
 
 		//Bug-130
 		String str="Good Evening..!!";
 		String str1="My code";
+
+		//int a=10;
+	    	//int b=20;
+		
+		add(10,20);
+
 
         String s="hi";
  
